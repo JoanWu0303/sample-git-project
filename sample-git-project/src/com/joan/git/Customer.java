296e4749198ffc680578714889603b513272a62b
Chih Chen Wu (Joan) <joanwu11226@gmail.com>
2023-04-08 17:18:28 -0400

    Update Customer.java

--- sample-git-project/src/com/joan/git/Customer.java
+++ sample-git-project/src/com/joan/git/Customer.java
@@ -19,6 +19,6 @@ public class Customer {
 	}
 	@Override
 	public String toString() {
-		return "Customer [name=" + name + ", age=" + age + "]";
+		return "Customer111 [name=" + name + ", age=" + age + "]";
 	}
 }
